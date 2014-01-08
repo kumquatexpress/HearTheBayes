e88a8ba9634742430824a5626381a4f03497f170
Boyang Niu <boyang.niu@gmail.com>
2014-01-08 11:31:27 -0500

    adding requirements and wsgi

---
+++ HearBayes.wsgi
@@ -0,0 +1,1 @@
+from hearmeroar import app as application
--- hearmeroar.py
+++ hearmeroar.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def base():
     return render_template("views/main_page.html")
+
+
+if __name__ == '__main__':
+    app.run()
